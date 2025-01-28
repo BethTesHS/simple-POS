c8c78c895eaa4043d9cd5b28428a12bb8b6af907
BethTesHS <bethelhemtesfaye95@gmail.com>
2025-01-28 10:43:33 +0300

    Sales Page Updated

--- resources/views/sales.blade.php
+++ resources/views/sales.blade.php
@@ -13,8 +13,8 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
 
     <?php
-        use App\Models\Product;
-        $products = Product::all();
+        use App\Models\Sale;
+        $sales = Sale::all();
     ?>
 </head>
 <body>
@@ -27,6 +27,21 @@
 
         function calcClosePopup(){
             calcPopup.style.display = 'none';
+        }
+
+        document.addEventListener('DOMContentLoaded', () => {
+            document.querySelectorAll('.detailView-btn').forEach(button => {
+                button.addEventListener('click', function() {
+                    const sales = JSON.parse(button.getAttribute('data-id'));
+                    const id = document.getElementById('con_id');
+                    id.value = sales['id'];
+                    document.getElementById('salesDetailPopup').style.display = 'flex';
+                });
+            });
+        });
+
+        function closeSalesPopupBtn() {
+            document.getElementById('salesDetailPopup').style.display = 'none'; // Hide the popup
         }
 
     </script>
@@ -52,6 +67,90 @@
         </div>
     </div>
 
+    <div class="wrapper">
+        <div class="sidebar">
+            <ul>
+                <li> <a href="\salesDetail"> <button class= "listButton"> <i class="i fa fa-handshake-o"></i> Sales </button> </a> </li>
+                <li> <a href="\salesAnalysis"> <button class= "listButton" > <i class="i fa fa-line-chart"></i> Analysis </button> </a> </li>
+            </ul>
+        </div>
+
+        <div class="mainviews" id="mainviews">
+
+            {{-- <div class="mainpage"> --}}
+                <div class="align">
+                    <header>
+                        <h1>Sales</h1>
+                    </header>
+                    {{-- <button class="addNewButton" id='addMember'><i class='fa fa-plus-square-o'></i> Add new member </button> --}}
+                </div>
+
+                <table>
+                    <thead>
+                    <tr>
+                        <th>Reciept ID</th>
+                        <th>Date</th>
+                        <th>Total Quantity</th>
+                        <th>Total Price</th>
+                        <th>Payment Detail</th>
+                        <th style="width: 10%"></th>
+                        {{-- <th colspan='2'></th> --}}
+                    </tr>
+                    </thead>
+                    <tbody>
+
+                        @foreach ($sales as $sale)
+                            <tr>
+                                <td> {{ sprintf("%010d", $sale->id) }} </td>
+                                <td> {{ $sale->created_at }} </td>
+                                <td> {{ $sale->totalQuantity }} </td>
+                                <td> {{ $sale->totalPrice }} </td>
+                                <td> {{ $sale->payMethod }} </td>
+                                <td style="padding: 0px; width: 60px;">
+                                    <button class="detailView-btn" data-id="{{ $sale }}">
+                                        {{-- <i style="width: 0px" class='fa fa-eye'></i>  --}}
+                                        View
+                                    </button>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+                @if($errors->any())
+                    <div class="alert alert-danger" style="color:red; padding-left: 20px;">
+                        <div>
+                            @foreach($errors->all() as $error)
+                            <br> <p>{{ $error }}</p>
+                            @endforeach
+                        </div>                        
+                    </div>
+                @endif
+            {{-- </div> --}}
+
+
+            {{-------------- // EDIT // --------------}}
+
+
+            <div id="salesDetailPopup" class="salesDetail">
+                <div class="salesDetail-content">
+                    <span class="close-btn" onclick="closeSalesPopupBtn()">&times;</span>
+                    <div style="padding: 20px 0px">
+                        <h3> Sales Detail </h3>
+                    </div>
+
+                    {{-- <form action="{{ route('contribution.update') }}" autocomplete="off" method="POST">
+                        @csrf
+                        @method('PUT') --}}
+
+                        <input type="text" id="con_id" class="textArea" name="id" type="text" value="" readonly>
+
+                        <input class="textButton" name="editMember" type="submit" value="Update Detail"> <br>
+                    {{-- </form> --}}
+                </div>
+            </div>
+
+        </div>
+    </div>
 
 
     @if($errors->any())
