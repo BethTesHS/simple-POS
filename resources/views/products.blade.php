cfcfb05717d1e68d5d9fe15e2b36627138ef184a
BethTesHS <bethelhemtesfaye95@gmail.com>
2025-01-29 14:52:24 +0300

    Fixed the look of dropdowns

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -95,7 +95,7 @@
         <div class="sidebar">
             <ul>
                 <li> <a href="\pos"> <button class= "listButton"> <i class="i fa fa-desktop"></i> POS </button> </a> </li>
-                <li> <a href="\products"> <button class= "listButton" > <i class="i fa fa-shopping-basket"></i> Product </button> </a> </li>
+                <li> <a href="\products"> <button style="color: white" class= "listButton" > <i class="i fa fa-shopping-basket"></i> Product </button> </a> </li>
                 <li> <a href="\sales"> <button class= "listButton"> <i class="i fa fa-shopping-cart"></i> Sales </button> </a> </li>
             </ul>
         </div>
--- resources/views/sales.blade.php
+++ resources/views/sales.blade.php
@@ -120,7 +120,7 @@
             <ul>
                 <li> <a href="\pos"> <button class= "listButton"> <i class="i fa fa-desktop"></i> POS </button> </a> </li>
                 <li> <a href="\products"> <button class= "listButton" > <i class="i fa fa-shopping-basket"></i> Product </button> </a> </li>
-                <li> <a href="\sales"> <button class= "listButton"> <i class="i fa fa-shopping-cart"></i> Sales </button> </a> </li>
+                <li> <a href="\sales"> <button style="color: white" class= "listButton"> <i class="i fa fa-shopping-cart"></i> Sales </button> </a> </li>
             </ul>
         </div>
 
